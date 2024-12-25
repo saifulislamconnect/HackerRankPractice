cc00e225ea3025845309e826cd305b4f83740fce
Saiful Islam <saifulislam.connect@gmail.com>
2024-12-26 03:52:53 +0600

    Initial Commit for hacker rank solution and practices

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using HackerRankPractice;
+
+SampleProgram.EntryMethod();
---
+++ SampleProgram.cs
@@ -0,0 +1,8 @@
+namespace HackerRankPractice;
+internal class SampleProgram
+{
+    public static void EntryMethod()
+    {
+        // Here goes all the main method contents
+    }
+}
